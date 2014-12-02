a8256da7d08612fbd4a2c01c67ad2e67c92a2d0c
Thomas Cate <thomascate@gmail.com>
2014-12-02 10:37:07 -0600

    I have no idea how that got doubled

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -10,7 +10,7 @@ from pprint import pprint
 
 #reddit api setup
 r = praw.Reddit(user_agent='toms scraperbot thomascate@gmail.com')
-submissions = r.get_subreddit('wallpaperdump').get_hot(limit=50)
+submissions = r.get_subreddit('wallpaperdump').get_hot(limit=10)
 
 #imgur api setup
 #creds are in imgurcreds.py
@@ -67,23 +67,28 @@ def get_imgur_ids_from_gallery(gallery):
 for post in submissions:
   sleep(1)
   #make sure it's a link
+#  pprint(vars(post))
   if not post.is_self:
 
     if get_url_type(post.url) == "directLink":
       directUrls['directLinks'].append(post.url)
 
     elif get_url_type(post.url) == "imgurAlbum":
-      albumID    = get_imgur_album_id(post.url)
-      if not imgurclient.get_album(albumID).title:
-        albumTitle = albumID
-      else:
-        albumTitle = imgurclient.get_album(albumID).title.replace("/","|")
-      print "scraping " + albumTitle
-      imgurIDs   = get_imgur_ids_from_album(albumID)
-      directUrls[albumTitle] = list()
+      try: 
+        albumID    = get_imgur_album_id(post.url)
+        if not imgurclient.get_album(albumID).title:
+          albumTitle = albumID
+        else:
+          albumTitle = imgurclient.get_album(albumID).title.replace("/","|")
+        print "scraping " + albumTitle
+        imgurIDs   = get_imgur_ids_from_album(albumID)
+        directUrls[albumTitle] = list()
 
-      for imgurID in imgurIDs:
-        directUrls[albumTitle].append( imgurID.link )
+        for imgurID in imgurIDs:
+          directUrls[albumTitle].append( imgurID.link )
+
+      except:
+        pass
        
     elif get_url_type(post.url) == "imgurGallery":
       print post.url
@@ -102,119 +107,14 @@ for key in directUrls:
     os.mkdir(key)
   for url in directUrls[key]:
     filename = key + '/' +  url.rsplit("/")[-1]
-    f = open(filename,'wb')
-    f.write(urllib2.urlopen(url).read())
-    f.close()
-#pprint(directUrls)
-#for image in imgurImages:
-#  print image
-
-
-#!/usr/bin/env python
-import praw
-from imgurpython import ImgurClient
-import imgurcreds
-import re
-import os
-import urllib2
-from pprint import pprint
-
-#reddit api setup
-r = praw.Reddit(user_agent='toms scraperbot thomascate@gmail.com')
-submissions = r.get_subreddit('wallpaperdump').get_hot(limit=5)
-
-#imgur api setup
-#creds are in imgurcreds.py
-imgurclient = ImgurClient(imgurcreds.clientID, imgurcreds.clientSecret)
-
-directUrls = { 'directLinks' : []}
-directUrls['directLinks'] = list()
-imgurAlbums = []
-imgurImages = []
-
-def get_url_type(url):
-  imgurMatch        = re.match( r'^(http|https)\:\/\/imgur\.com', url, re.M|re.I)
-  imgurAlbumMatch   = re.match( r'^(http|https)\:\/\/imgur\.com\/(a|gallery)\/', url, re.M|re.I)
-  #imgurGalleryMatch = re.match( r'^(http|https)\:\/\/imgur\.com\/gallery\/', url, re.M|re.I)
-  directLink        = re.match( r'.*\.jpg', url, re.M|re.I)
-
-  if directLink:
-  	urlType = "directLink"
-  elif imgurAlbumMatch:
-    urlType = "imgurAlbum"
-  #elif imgurGalleryMatch:
-    #urlType = "imgurGallery"
-  elif imgurMatch:
-  	urlType = "imgur"
-  else:
-  	urlType = "unknown"
-
-  return urlType
-
-def get_imgur_album_id(url):
-  return url.rsplit("/")[4][0:5]
-
-def get_imgur_ids_from_url(url):
-  #these posts often contain lists of ids
-  idPath = url.rsplit("/")[3]
-  ids = []
-
-  if "," in idPath:
-    for imgurID in idPath.rsplit(","):
-      ids.append(imgurID[0:5])
-
-  else:
-    ids.append(idPath[0:5])
-
-  return ids
-
-def get_imgur_ids_from_album(album):
-  albumImages = imgurclient.get_album_images(album)
-  return albumImages
-
-def get_imgur_ids_from_gallery(gallery):
-  return True
-
-for post in submissions:
-
-  #make sure it's a link
-  if not post.is_self:
-
-    if get_url_type(post.url) == "directLink":
-      directUrls['directLinks'].append(post.url)
-
-    elif get_url_type(post.url) == "imgurAlbum":
-      albumID    = get_imgur_album_id(post.url)
-      if not imgurclient.get_album(albumID).title:
-        albumTitle = 'directLinks'
-      else:
-        albumTitle = imgurclient.get_album(albumID).title.replace("/","|")
-      imgurIDs   = get_imgur_ids_from_album(albumID)
-      directUrls[albumTitle] = list()
-
-      for imgurID in imgurIDs:
-        directUrls[albumTitle].append( imgurID.link )
-       
-    elif get_url_type(post.url) == "imgurGallery":
-      print post.url
-
-    elif get_url_type(post.url)  == "imgur":
-      imgurImages = imgurImages + get_imgur_ids_from_url(post.url)
-
-
-for album in imgurAlbums:
-  print album
-
-for key in directUrls:
-  print key
-  print len(directUrls[key])
-  if not os.path.exists(key):
-    os.mkdir(key)
-  for url in directUrls[key]:
-    filename = key + '/' +  url.rsplit("/")[-1]
-    f = open(filename,'wb')
-    f.write(urllib2.urlopen(url).read())
-    f.close()
+    print "getting url " + url
+    print "filename " +  filename
+    try:
+      f = open(filename,'wb')
+      f.write(urllib2.urlopen(url).read())
+      f.close()
+    except:
+      pass
 #pprint(directUrls)
 #for image in imgurImages:
 #  print image
